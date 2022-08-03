39f8a1423199980fc918018c23cc6ce102fe2b67
Dave Fisher <dave.j.fish@gmail.com>
2022-08-03 16:13:52 -0700

    tests written for specific smt game detail

--- __tests__/smt.test.js
+++ __tests__/smt.test.js
@@ -7,11 +7,23 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
   it('#POST /smt should return list of smt games', async () => {
     const response = await request(app).get('/smt');
     expect(response.status).toBe(200);
     expect(response.body.length).toEqual(5);
   });
+
+  it('#POST smt/:id should return an smt game', async () => {
+    const response = await request(app).get('/smt/1');
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      id: '1',
+      title: 'shin megami tensei 1',
+      platform: 'super famicom',
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
